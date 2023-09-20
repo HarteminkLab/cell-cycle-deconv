444ded9cb9a73877de019fa9dba800669748a853
Trung Tran <trung.tran@duke.edu>
2023-09-19 21:06:31 -0400

    Some script updates to clean up plotting

--- deconvolveGene.m
+++ deconvolveGene.m
@@ -11,26 +11,5 @@ function [model] = deconvolveGene(genename)
     fprintf("Deconvolving %s...", genename);
     model = deconvolve(model);
     fprintf("Done.\n");
-    
-    % Make the plotting directory
-    plottingdir = 'output/plotting';
-    try
-        mkdir(plottingdir);
-    catch
-        % Skip, make the directory silently
-    end
-    
-    fprintf("The rn of the model is: %.4f\n", model.rn);
-    fprintf("The sn of the model is: %.4f\n", model.sn);
-    
-    % Plot the results
-    fprintf("Plotting...");
-    fig = drawDeconvolved(model);
-    savename = sprintf('%s/%s.png', plottingdir, genename);
-    saveas(fig, savename); 
-
-    % close;
-
-    fprintf("Done, saved to %s\n", savename);
 
 end
--- mainSingleGenes.m
+++ mainSingleGenes.m
@@ -3,6 +3,8 @@ addpath(genpath('lib/YAMLMatlab'));
 addpath(genpath('deconv'))
 addpath(genpath('analysis'))
 
-outdir = 'output/';e
+outdir = 'output/';
+plottingdir = 'output/plotting';
 
 [model] = deconvolveGene('CLB2');
+[model] = plotDeconvolved(model, plottingdir);
