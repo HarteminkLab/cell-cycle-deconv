24835a5b51f2a8f98af251ae27ea03c5583ce208
Trung Tran <trung.tran@duke.edu>
2023-10-02 15:34:02 -0400

    Add saving H matrix to output

--- main.m
+++ main.m
@@ -1,64 +1,70 @@
+function [] = main()
 
-addpath(genpath('lib/YAMLMatlab'));
-addpath(genpath('deconv'))
-addpath(genpath('analysis'))
+    outdir = 'output/2023-09-26_yl_rep2_all_genes_rg1/';
+    
+    addpath(genpath('lib/YAMLMatlab'));
+    addpath(genpath('deconv'))
+    addpath(genpath('analysis'))
 
-outdir = 'output/2023-09-26_yl_rep2_all_genes_rg1/';
+    % Deconvolve 1 gene so we can get the dimensions of f and g easily, rather 
+    % than digging through the parsing code of the model file
+    model = Model('CLB2', 0.004);
+    model = deconvolve(model);
 
-% Deconvolve 1 gene so we can get the dimensions of f and g easily, rather 
-% than digging through the parsing code of the model file
-model = Model('CLB2', 0.004);
-model = deconvolve(model);
-
-% Start deconvolve of all genes
-[stand_sys2pos] = textread(strcat(Deconv.DECONV_DATASET, 'genes.lst'), '%s');
-
-numgenes = size(stand_sys2pos, 1);
-num_f = size(model.f, 1);
-all_genes_f = zeros(numgenes, num_f);
-
-num_g = size(model.g, 2);
-all_genes_g = zeros(numgenes, num_g);
-
-% Start the timer
-tic;
-
-gamma_val = 0.004;
-
-numerror = 0;
-numsuccess = 0;
-for index = 1:length(stand_sys2pos)
-
-    orf_name = stand_sys2pos{index};
-
-    elapsedTime = toc;
-    fprintf("Deconvolving %s (%d/%d) - %.3f min\n", orf_name, index, size(stand_sys2pos, 1), ...
-        elapsedTime/60.);
-
-    try
-        model = Model(orf_name, gamma_val);
-        all_genes_g(index, :) = model.g;
-
-        model = deconvolve(model);
-        all_genes_f(index, :) = model.f;
-
-        numsuccess = numsuccess + 1;
-    catch
-        fprintf("   There was an error trying to deconvolve this gene, let's skip it.\n");
-        numerror = numerror + 1;
-        continue;
+    writematrix(model.H, strcat(outdir, '/H.csv'));
+    
+    % Start deconvolve of all genes
+    [stand_sys2pos] = textread(strcat(Deconv.DECONV_DATASET, 'genes.lst'), '%s');
+    
+    numgenes = size(stand_sys2pos, 1);
+    num_f = size(model.f, 1);
+    all_genes_f = zeros(numgenes, num_f);
+    
+    num_g = size(model.g, 2);
+    all_genes_g = zeros(numgenes, num_g);
+    
+    % Start the timer
+    tic;
+    
+    gamma_val = 0.004;
+    
+    numerror = 0;
+    numsuccess = 0;
+    for index = 1:length(stand_sys2pos)
+    
+        orf_name = stand_sys2pos{index};
+    
+        elapsedTime = toc;
+        fprintf("Deconvolving %s (%d/%d) - %.3f min\n", orf_name, index, size(stand_sys2pos, 1), ...
+            elapsedTime/60.);
+    
+        try
+            model = Model(orf_name, gamma_val);
+            all_genes_g(index, :) = model.g;
+    
+            model = deconvolve(model);
+            all_genes_f(index, :) = model.f;
+    
+            numsuccess = numsuccess + 1;
+        catch
+            fprintf("   There was an error trying to deconvolve this gene, let's skip it.\n");
+            numerror = numerror + 1;
+            continue;
+        end
+    
+        % Stop the timer
+        elapsedTime = toc;
+    
+        % Periodically save the output filescomcomc
+        if mod(index, 10) == 0
+            writedata(outdir, all_genes_f, all_genes_g, stand_sys2pos);
+        end
     end
-
-    % Stop the timer
-    elapsedTime = toc;
-
-    % Periodically save the output filescomcomc
-    if mod(index, 10) == 0
-        writedata(outdir, all_genes_f, all_genes_g, stand_sys2pos);
-    end
+    
+    writedata(outdir, all_genes_f, all_genes_g, stand_sys2pos);
+    
+    fprintf("Done. Completed in %.3f min\n", elapsedTime/60.);
 end
-
-writedata(outdir, all_genes_f, all_genes_g, stand_sys2pos);
 
 function[] = writedata(outdir, all_genes_f, all_genes_g, stand_sys2pos)
 
--- mainSingleGenes.m
+++ mainSingleGenes.m
@@ -6,6 +6,8 @@ addpath(genpath('analysis'))
 outdir = 'output/';
 plottingdir = 'output/plotting';
 
-model = Model('SSK22', 0.004);
+model = Model('CLB2', 0.004);
 model = deconvolve(model);
 drawDeconvolvedRG1(model, plottingdir);
+
+writematrix(model.H, strcat(outdir, '/H.csv'));
