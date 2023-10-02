7b2f2046bed3449b3239c4c993706071d9bbbe9e
Trung Tran <trung.tran@duke.edu>
2023-10-02 15:55:22 -0400

    Testing a version of the RG1 model where we reduce the sigma0 value, seems to greatly improve the fits...

--- mainSingleGenes.m
+++ mainSingleGenes.m
@@ -6,8 +6,7 @@ addpath(genpath('analysis'))
 outdir = 'output/';
 plottingdir = 'output/plotting';
 
-model = Model('CLB2', 0.004);
+model = Model('SSK22', 0.004);
 model = deconvolve(model);
 drawDeconvolvedRG1(model, plottingdir);
 
-writematrix(model.H, strcat(outdir, '/H.csv'));
