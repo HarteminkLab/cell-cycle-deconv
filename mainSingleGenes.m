4d0e7af57f5cf045289752f238531cf62561c38c
Trung Tran <trung.tran@duke.edu>
2023-11-07 12:22:02 -0500

    Working run of yl2 data, yl2 model with deconv2 deconvolution code from granovskaia. Next todo, is port over the padding logic from the refactor. The rn looks reasonable in this run ~1.5 as opposed to 700-800s

--- mainSingleGenes.m
+++ mainSingleGenes.m
@@ -6,8 +6,8 @@ addpath(genpath('analysis'))
 outdir = 'output/';
 plottingdir = 'output/plotting';
 
-model = Model('CLN2', 0.196);
-model = deconvolve(model);
-drawDeconvolvedRG1(model, plottingdir);
+model = Model('CLN2', 0.04);
+model = deconvModel(model);
 
-writematrix(model.H, strcat(outdir, '/H.csv'));
+% drawDeconvolvedRG1(model, plottingdir);
+%writematrix(model.H, strcat(outdir, '/H.csv'));
