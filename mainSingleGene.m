af90d79df25813e8fc14a7ccadb8b915852e428b
Trung Tran <trung.tran@duke.edu>
2023-11-10 13:29:41 -0500

    Some clean up for a working single gene

--- mainSingleGene.m
+++ mainSingleGene.m
@@ -3,6 +3,7 @@ addpath(genpath('lib/YAMLMatlab'));
 addpath(genpath('deconv'))
 addpath(genpath('analysis'))
 
+
 outdir = 'output/';
 plottingdir = 'output/plotting';
 
@@ -13,7 +14,7 @@ drawDeconvolvedRG1(model, plottingdir);
 
 % --------------- Find optimal gamma through elbow method ------------
 
-[gammas, rn, sn, elbow_gamma] = findOptimal(model, true);
+[model, flag, rn, sn, gammas, elbow_gamma] = findOptimal(model, true);
 
 figure;
 
@@ -31,27 +32,6 @@ ylabel('smooth error');
 axis square;
 
 % ------------- Curvature --------------
-
-x_grad1 = gradient(rn);
-x_grad2 = gradient(x_grad1);
-y_grad1 = gradient(sn);
-y_grad2 = gradient(y_grad1);
-curvature = (x_grad1.*y_grad2-y_grad1.*x_grad2) ./ ((x_grad1.^2+y_grad1.^2).^(1.5));
-
-boundary = 1;
-
-fprintf("The x_grad1 is: %f\n", x_grad1);
-fprintf("The x_grad2 is: %f\n", x_grad2);
-fprintf("The curvature is: %f\n", curvature);
-
-[max_val, max_pos] = max(curvature(boundary+1:length(rn)-boundary));
-max_pos = max_pos+boundary;
-pos_left = max_pos-boundary;
-pos_right = max_pos+boundary;
-elbow_gamma = gammas(max_pos);
-
-figure;
-plot(x_grad1);
 
 
 
