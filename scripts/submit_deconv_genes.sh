938f61986a592f0ba1aec5a0ea1d95c922a35f99
Trung Tran <trung.tran@duke.edu>
2023-12-12 21:15:50 -0500

    Update script for looping through orf names to submit jobs

--- scripts/submit_deconv_genes.sh
+++ scripts/submit_deconv_genes.sh
@@ -9,9 +9,26 @@
     #sbatch -D ./slurm-logs/ --job-name="ccplt_$CHROM" --export="PYFILE=src/gene_plotting_runner.py,ARGS=$CHROM" scripts/cpu_job.sh
 #done
 
+
+
 # Testing one job
 OUTDIR=output/deconvolve_chromatin_2023_12_12
-ORFNAME=YPR119W
-i=0
+#ORFNAME=YPR119W
 
-sbatch -D ./slurm-logs/ --job-name="deconv_$i" --export="PYFILE=src/deconvolve_chromatin_runner.py,ARGS=$OUTDIR,$ORFNAME" scripts/cpu_job.sh
+#sbatch -D ./slurm-logs/ --job-name="deconv_$i" --export="PYFILE=src/deconvolve_chromatin_runner.py,ARGS=$OUTDIR,$ORFNAME" scripts/cpu_job.sh
+
+# Loop through the ORF names and create a slurm job to deconvolve each of these genes
+i=1
+while IFS= read -r line
+do
+    if [ "$line" != "orf_name" ]; then
+
+        ORFNAME=$line
+        sbatch -D ./slurm-logs/ --job-name="deconv_$i" --export="PYFILE=src/deconvolve_chromatin_runner.py,ARGS=$OUTDIR,$ORFNAME" scripts/cpu_job.sh
+        i=$(($i+1))
+        break
+
+    fi
+
+# We are reading in the first column of the csv using cut ("-f1") which defines the orf name
+done < <(cut -d ',' -f1 data/reference_data/geneset_nondub_w_prom_genebodies.csv)
