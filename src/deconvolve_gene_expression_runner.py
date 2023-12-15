c308d81d961db6f4f2185f529d78afb67916bfb3
Trung Tran <trung.tran@duke.edu>
2023-12-15 16:41:37 -0500

    Notebook organization and cleanup

--- src/deconvolve_gene_expression_runner.py
+++ src/deconvolve_gene_expression_runner.py
@@ -7,8 +7,7 @@ import pandas as pd
 from src.timer import Timer
 from src.model import Model
 from src.find_gamma_chromatin import FindOptimalGammaChromatin
-from src.config import load_yl_replicate2_rg1_chromatin_config
-from cc_src.chromatin_grid_compute import ChromatinGrid
+from src.config import load_yl_replicate2_rg1_config
 
 
 def main():
@@ -29,7 +28,7 @@ def main():
 
 	gene = geneset.iloc[gene_index]
 
-	config = load_yl_replicate2_rg1_chromatin_config()
+	config = load_yl_replicate2_rg1_config()
 	model = Model(config, gene.orf_name, 0.001)
 
 	print(f"Index: [{gene_index}/{len(geneset)}] Deconvolving gene: {model.gene_name}/{model.orf_name}...")
@@ -38,24 +37,16 @@ def main():
 	# Initialize the chromatin grid
 	timer = Timer()
 
-	chromatin_gridder = ChromatinGrid()
+	# find_gamma_chromatin = FindOptimalGammaChromatin(model, chromatin_gridder)
+	# found_optimal_success = find_gamma_chromatin.find_optimal()
+	# using_default_flag = not found_optimal_success
 
-	if model.gene_name is None: chromatin_gridder.set_gene(model.orf_name)
-	else: chromatin_gridder.set_gene(model.gene_name)
+	# f = find_gamma_chromatin.f
 
-	chromatin_gridder.create_bins_per_all_sample()
-	chromatin_gridder.create_deconvolution_matrices()
+	# print(f"Finished finding the optimal gamma in : {timer.get_time()}")
+	# sys.stdout.flush()
 
-	find_gamma_chromatin = FindOptimalGammaChromatin(model, chromatin_gridder)
-	found_optimal_success = find_gamma_chromatin.find_optimal()
-	using_default_flag = not found_optimal_success
-
-	f = find_gamma_chromatin.f
-
-	print(f"Finished finding the optimal gamma in : {timer.get_time()}")
-	sys.stdout.flush()
-
-	chromatin_gridder.save_deconvolved_outputs(out_dir, index, model, f, using_default_flag)
+	# chromatin_gridder.save_deconvolved_outputs(out_dir, index, model, f, using_default_flag)
 
 
 if __name__ == '__main__':
