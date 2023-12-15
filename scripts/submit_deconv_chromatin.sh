5132bcf3c75efc7900317e28130893150f38ff76
Trung Tran <trung.tran@duke.edu>
2023-12-15 16:58:49 -0500

    Submit all genes

--- scripts/submit_deconv_chromatin.sh
+++ scripts/submit_deconv_chromatin.sh
@@ -8,10 +8,10 @@ OUTDIR=output/deconvolve_chromatin_2023_12_15
 
 # Each batch will run 1000 genes, second argument in ARGS is the batch index that will be multiplied against the
 # array index
-sbatch -a 0-999%10 -D ./slurm-logs/ --job-name="dchrom" -p compsci --export="PYFILE=src/deconvolve_chromatin_runner.py,ARGS=$OUTDIR 0" scripts/cpu_array_job.sh
-sbatch -a 0-999%10 -D ./slurm-logs/ --job-name="dchrom" -p compsci --export="PYFILE=src/deconvolve_chromatin_runner.py,ARGS=$OUTDIR 1" scripts/cpu_array_job.sh
-sbatch -a 0-999%10 -D ./slurm-logs/ --job-name="dchrom" -p compsci --export="PYFILE=src/deconvolve_chromatin_runner.py,ARGS=$OUTDIR 2" scripts/cpu_array_job.sh
-#sbatch -a 0-999%10 -D ./slurm-logs/ --job-name="dchrom" -p compsci --export="PYFILE=src/deconvolve_chromatin_runner.py,ARGS=$OUTDIR 3" scripts/cpu_array_job.sh
-#sbatch -a 0-999%10 -D ./slurm-logs/ --job-name="dchrom" -p compsci --export="PYFILE=src/deconvolve_chromatin_runner.py,ARGS=$OUTDIR 4" scripts/cpu_array_job.sh
-#sbatch -a 0-773%10 -D ./slurm-logs/ --job-name="dchrom" -p compsci --export="PYFILE=src/deconvolve_chromatin_runner.py,ARGS=$OUTDIR 5" scripts/cpu_array_job.sh
+sbatch -a 0-999%6 -D ./slurm-logs/ --job-name="dchrom" -p compsci --export="PYFILE=src/deconvolve_chromatin_runner.py,ARGS=$OUTDIR 0" scripts/cpu_array_job.sh
+sbatch -a 0-999%6 -D ./slurm-logs/ --job-name="dchrom" -p compsci --export="PYFILE=src/deconvolve_chromatin_runner.py,ARGS=$OUTDIR 1" scripts/cpu_array_job.sh
+sbatch -a 0-999%6 -D ./slurm-logs/ --job-name="dchrom" -p compsci --export="PYFILE=src/deconvolve_chromatin_runner.py,ARGS=$OUTDIR 2" scripts/cpu_array_job.sh
+sbatch -a 0-999%6 -D ./slurm-logs/ --job-name="dchrom" -p compsci --export="PYFILE=src/deconvolve_chromatin_runner.py,ARGS=$OUTDIR 3" scripts/cpu_array_job.sh
+sbatch -a 0-999%6 -D ./slurm-logs/ --job-name="dchrom" -p compsci --export="PYFILE=src/deconvolve_chromatin_runner.py,ARGS=$OUTDIR 4" scripts/cpu_array_job.sh
+sbatch -a 0-773%3 -D ./slurm-logs/ --job-name="dchrom" -p compsci --export="PYFILE=src/deconvolve_chromatin_runner.py,ARGS=$OUTDIR 5" scripts/cpu_array_job.sh
 
