d6d8d076348c21a65d8e2ee50926bc4f1f6f9c37
Trung Tran <trung.tran@duke.edu>
2023-12-15 12:50:23 -0500

    Add comment on current constraint to submitting jobs to the cluster

--- scripts/submit_deconv_genes.sh
+++ scripts/submit_deconv_genes.sh
@@ -3,7 +3,13 @@
 
 OUTDIR=output/deconvolve_chromatin_2023_12_13
 
-# There are 5774 in total, so 5774 jobs. We will try 20 at a time
-# They take around 20 minutes, so let's try 20 at a time
-SBATCH -a 1-5774%20 -D ./slurm-logs/ -p compsci --export="PYFILE=src/deconvolve_chromatin_runner.py,ARGS=$OUTDIR" scripts/cpu_array_job.sh
+# There are 5774 in total, so 5774 jobs
+
+# Max Array size is 1001, so let's do 6 super-batches
+# sbatch -a 0-1000%10 -D ./slurm-logs/ -p compsci --export="PYFILE=src/deconvolve_chromatin_runner.py,ARGS=$OUTDIR" scripts/cpu_array_job.sh
+
+# TODO:
+# Current constraint to work around, max array size is 1001, meaning we can't specify an array from 1001-2000,
+# So we'll need to add another argument to the python runner to multiply the array index to the correct orf...
+#
 
