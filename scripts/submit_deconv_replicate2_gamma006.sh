7cd103b7c841f9d28f92c8e8f8f01304ed1f76b4
Trung Tran <trung.tran@duke.edu>
2024-03-08 15:35:08 -0500

    Update replicate runs for dg1 fix

--- scripts/submit_deconv_replicate2_gamma006.sh
+++ scripts/submit_deconv_replicate2_gamma006.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Script to create jobs to create locus plots for all genes across all chromosomes
 
-OUTDIR=output/deconvolve_rep2_g006_2024_02_22
+OUTDIR=output/deconvolve_rep2_g006_2024_03_08_dg1_fix
 
 # There are 5774 in total, so 5774 jobs. We will split them into 1000 batch jobs (because of a limitation on the size
 # of the array on the slurm computing cluster. Therefore we also need a batch argument in ARGS)
@@ -14,4 +14,3 @@ sbatch -a 0-999%3 -D ./slurm-logs/ --job-name="dec2_2" -p compsci --export="PYFI
 sbatch -a 0-999%3 -D ./slurm-logs/ --job-name="dec2_3" -p compsci --export="PYFILE=src/deconvolve_runner.py,ARGS=$OUTDIR 2 0.006,BATCH=3" scripts/cpu_genearray_job.sh
 sbatch -a 0-999%3 -D ./slurm-logs/ --job-name="dec2_4" -p compsci --export="PYFILE=src/deconvolve_runner.py,ARGS=$OUTDIR 2 0.006,BATCH=4" scripts/cpu_genearray_job.sh
 sbatch -a 0-773%3 -D ./slurm-logs/ --job-name="dec2_5" -p compsci --export="PYFILE=src/deconvolve_runner.py,ARGS=$OUTDIR 2 0.006,BATCH=5" scripts/cpu_genearray_job.sh
-
