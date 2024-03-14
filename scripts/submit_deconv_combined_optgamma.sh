2ae8432f313b42c9f05bf6cb9f6179bbde4f6df9
Trung Tran <trung.tran@duke.edu>
2024-03-14 15:21:40 -0400

    Fix bug to add comma to parameters

--- scripts/submit_deconv_combined_optgamma.sh
+++ scripts/submit_deconv_combined_optgamma.sh
@@ -8,9 +8,10 @@ OUTDIR=output/deconvolve_combined_g_opt_2024_03_14
 
 # Each batch will run 1000 genes, second argument in ARGS is the batch index that will be multiplied against the
 # array index
-sbatch -a 0-999%6 -D ./slurm-logs/ --job-name="dcomb_1" -p compsci --export="PYFILE=src/deconvolve_combined_runner.py,ARGS=$OUTDIR BATCH=0" scripts/cpu_genearray_job.sh
-sbatch -a 0-999%6 -D ./slurm-logs/ --job-name="dcomb_2" -p compsci --export="PYFILE=src/deconvolve_combined_runner.py,ARGS=$OUTDIR BATCH=1" scripts/cpu_genearray_job.sh
-sbatch -a 0-999%6 -D ./slurm-logs/ --job-name="dcomb_3" -p compsci --export="PYFILE=src/deconvolve_combined_runner.py,ARGS=$OUTDIR BATCH=2" scripts/cpu_genearray_job.sh
-sbatch -a 0-999%6 -D ./slurm-logs/ --job-name="dcomb_4" -p compsci --export="PYFILE=src/deconvolve_combined_runner.py,ARGS=$OUTDIR BATCH=3" scripts/cpu_genearray_job.sh
-sbatch -a 0-999%6 -D ./slurm-logs/ --job-name="dcomb_5" -p compsci --export="PYFILE=src/deconvolve_combined_runner.py,ARGS=$OUTDIR BATCH=4" scripts/cpu_genearray_job.sh
-sbatch -a 0-773%6 -D ./slurm-logs/ --job-name="dcomb_6" -p compsci --export="PYFILE=src/deconvolve_combined_runner.py,ARGS=$OUTDIR BATCH=5" scripts/cpu_genearray_job.sh
+sbatch -a 0-999%6 -D ./slurm-logs/ --job-name="dcomb_1" -p compsci --export="PYFILE=src/deconvolve_combined_runner.py,ARGS=$OUTDIR,BATCH=0" scripts/cpu_genearray_job.sh
+sbatch -a 0-999%6 -D ./slurm-logs/ --job-name="dcomb_2" -p compsci --export="PYFILE=src/deconvolve_combined_runner.py,ARGS=$OUTDIR,BATCH=1" scripts/cpu_genearray_job.sh
+sbatch -a 0-999%6 -D ./slurm-logs/ --job-name="dcomb_3" -p compsci --export="PYFILE=src/deconvolve_combined_runner.py,ARGS=$OUTDIR,BATCH=2" scripts/cpu_genearray_job.sh
+sbatch -a 0-999%6 -D ./slurm-logs/ --job-name="dcomb_4" -p compsci --export="PYFILE=src/deconvolve_combined_runner.py,ARGS=$OUTDIR,BATCH=3" scripts/cpu_genearray_job.sh
+sbatch -a 0-999%6 -D ./slurm-logs/ --job-name="dcomb_5" -p compsci --export="PYFILE=src/deconvolve_combined_runner.py,ARGS=$OUTDIR,BATCH=4" scripts/cpu_genearray_job.sh
+sbatch -a 0-773%6 -D ./slurm-logs/ --job-name="dcomb_6" -p compsci --export="PYFILE=src/deconvolve_combined_runner.py,ARGS=$OUTDIR,BATCH=5" scripts/cpu_genearray_job.sh
+
