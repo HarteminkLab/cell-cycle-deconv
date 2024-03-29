60 min fix and dna content scaling for s updated
- Recompute the mnase data files for the fixed 60 minute timepoint for rep1
- Updated the logic for computing the multiplier on S phase cells for the raw data normalization

cc_src/mnase_replication_timing_analysis.py
@@ -61,7 +61,7 @@ class MNaseOriginAnalysis:
 		# Generate the start indices for each window
 		self.num_windows = (counts.size - window_size) // step + 1
 		self.start_indices = np.arange(self.num_windows) * step
-		self.window_counts = sliding_window_approach(counts, window_size, step)
+		self.window_counts = compute_sliding_window(counts, window_size, step)
 
 	def normalize_samples(self):
 		"""Normalize by expected DNA content at each time point"""
@@ -271,7 +271,7 @@ class MNaseOriginAnalysis:
 
 
 
-def sliding_window_approach(data, window_size, step):
+def compute_sliding_window(data, window_size, step):
 	# Number of windows
 	n_windows = (len(data) - window_size) // step + 1
 	

src/estimate_dna_content.py
@@ -17,10 +17,15 @@ from math import comb
 # The initial population mass, used in the Qr and Mgr calculations
 START = 1000
 
-# Maximum number of cell cycle "runs"
-MAX_RUNS = 10
+# Maximum number of cell cycle "c"
+MAX_CELL_CYCLES = 10
 
-def calcH_unnormalized(model_intervals, timepoints, normalize_mass=True, est_dna_content_mul=False):
+def calcH_unnormalized(config, normalize_mass=True, est_dna_content_mul=False):
+
+	model_intervals = config.intervals_wt1
+	timepoints = config.WT1_TIMEPOINTS
+	branch_phase_mapping = config.get_branch_phase_mapping()
+
 	parameters, relations, initial_timepoints, top_timepoints, bottom_timepoints, _ = model_intervals
 
 	if len(parameters) == 8:
@@ -35,63 +40,99 @@ def calcH_unnormalized(model_intervals, timepoints, normalize_mass=True, est_dna
 	# For each timepoint in the experiment, (rows in g)
 	for i, t in enumerate(timepoints):
 
+		# the positional distribution parameters
+		mean_g = t-mu0
+		sd_g = np.sqrt(sigma0**2 + t**2 * sigmav**2)
+
 		Q = 0
 
 		# Compute the Qr value or mass at a given timepoint in the experiment
 		# We are doing this for each run (cell cycle)
-		for r in range(MAX_RUNS + 1):
-			Q += Qr(mu0, sigma0, sigmav, delta, lambda_val, t, r, alpha)
+		for c in range(MAX_CELL_CYCLES + 1):
+			Q += Qr(mu0, sigma0, sigmav, delta, lambda_val, t, c, alpha)
 
 		# We also want to have a fraction of the initial population, so t=0
 		# over the expected mass at our current time
 		frac_init = Qr(mu0, sigma0, sigmav, delta, lambda_val, t, 0, alpha) / Q
 
+		# ------------------------- Contruct H from CDFs ----------------------------------------------
+
+		#     ------ {0, 0} initial cohort first cell cycle (c=0) ------
+
 		# Now we will construct our initial branch's columns
 		# Enumerate through the timepoints of the initial branch
 		for idx, tp in enumerate(initial_timepoints):
 
+			phase = branch_phase_mapping['i'][idx]
+
 			# Compute the cdf for the initial branch timepoint
-			cdf = norm.cdf(tp, loc=t-mu0, scale=np.sqrt(sigma0**2 + t**2 * sigmav**2))
-			initial_partial_H[idx][i, :] = np.diff(cdf) * frac_init
+			cdf = norm.cdf(tp, loc=mean_g, scale=sd_g)
+
+			# Calculate the difference between cdf terms for each timepoint
+			# and multiply against the fraction of cells in the
+			# current cell cycle
+			cur_partial_H = np.diff(cdf) * frac_init
+			initial_partial_H[idx][i, :] = cur_partial_H
+
+		#     ------ {0, 0} initial cohort becomes top branch (c=1+) ------
 
 		# For the top timepoint, we will be computing the cdf
 		# to compute the mass for each timepoint interval
 		# i.e.   CG1, and postG1
 		# this is for the first cohort and cell cycle {0, 0}
-		for runs in range(1, MAX_RUNS + 1):
+		for c in range(1, MAX_CELL_CYCLES + 1):
 
 			# Enumerate through the timepoints for each subinterval belonging to the to top timepoints
 			for idx, tp in enumerate(top_timepoints):
 
+				phase = branch_phase_mapping['t'][idx]
+
 				# Compute the cdf for the top branch timepoint
-				cdf = norm.cdf(tp + runs * lambda_val, loc=t-mu0, scale=np.sqrt(sigma0**2 + t**2 * sigmav**2))
-				top_partial_H[idx][i, :] += np.diff(cdf) * frac_init
+				cdf = norm.cdf(tp + c * lambda_val, loc=mean_g, scale=sd_g)
+
+				cur_partial_H = np.diff(cdf) * frac_init
+				top_partial_H[idx][i, :] += cur_partial_H
+
+		#     ---------------- top and bottom branches for {1+, 1+} cohorts (c=1+) ------------------------
 
 		# This computes the cohorts for the other cohorts {1+, 1+}
 		# For the top and bottom branches
-		for r in range(1, MAX_RUNS + 1):
+		for r in range(1, MAX_CELL_CYCLES + 1):
 			for g in range(1, r + 1):
 
 				# How much mass is there for the current cohort
 				frac_rest = Mgr(mu0, sigma0, sigmav, delta, lambda_val, t, g, r, alpha) / Q
 
 				if frac_rest > 1e-10:
-					trun_cdf = norm.cdf(r * lambda_val + (g-1) * delta - alpha, loc=t-mu0, 
-						scale=np.sqrt(sigma0**2 + t**2 * sigmav**2))
+					trun_cdf = norm.cdf(r * lambda_val + (g-1) * delta - alpha, loc=mean_g, 
+						scale=sd_g)
 					trun_denom = 1 - trun_cdf
 
 					for idx, tp in enumerate(top_timepoints):
-						for runs in range(r + 1, MAX_RUNS + 1):
-							cdf = norm.cdf(tp + runs * lambda_val + g * delta, loc=t-mu0, 
-									scale=np.sqrt(sigma0**2 + t**2 * sigmav**2))
+						
+						phase = branch_phase_mapping['t'][idx]
+
+						for c in range(r + 1, MAX_CELL_CYCLES + 1):
+
+							cdf = norm.cdf(tp + c * lambda_val + g * delta, loc=mean_g, 
+									scale=sd_g)
 							portion = cdf * 0 if trun_denom == 0 else (cdf - trun_cdf) / trun_denom
-							top_partial_H[idx][i, :] += np.diff(portion) * frac_rest
+
+							cur_partial_H = np.diff(portion) * frac_rest
+							top_partial_H[idx][i, :] += cur_partial_H
+
 
 					for idx, tp in enumerate(bottom_timepoints):
-						cdf = norm.cdf(tp + r * lambda_val + g * delta, loc=t-mu0, 
-							scale=np.sqrt(sigma0**2 + t**2 * sigmav**2))
+
+						phase = branch_phase_mapping['b'][idx]
+
+						cdf = norm.cdf(tp + r * lambda_val + g * delta, loc=mean_g, 
+							scale=sd_g)
 						portion = cdf * 0 if trun_denom == 0 else (cdf - trun_cdf) / trun_denom
-						bottom_partial_H[idx][i, :] += np.diff(portion) * frac_rest
+
+						cur_partial_H = np.diff(portion) * frac_rest
+						bottom_partial_H[idx][i, :] += cur_partial_H
+
 
 	Hsegments = {}
 	for i in range(len(relations)):
@@ -140,14 +181,18 @@ def calcH_unnormalized(model_intervals, timepoints, normalize_mass=True, est_dna
 		# Add the halted cells proportion as the last column
 		H_w_halted[i, -1] = halted/denom
 
-
 	# todo: Currently indices 2 and 3 are S and G2M
 	s_indices = Hpos[3]
 	g2m_indices = Hpos[4]
 
 	if est_dna_content_mul:
 
-		H_w_halted[:, s_indices[0]:s_indices[1]] *= 1.5
+		# Compute what the scalar should be as cells progress
+		# through S phase (the S phase columns in the H matrix)
+		s_tps = config.intervals_wt1[2][1]
+		dna_scalar = compute_dna_content_s(config, s_tps[:-1])
+
+		H_w_halted[:, s_indices[0]:s_indices[1]] *= dna_scalar
 		H_w_halted[:, g2m_indices[0]:g2m_indices[1]] *= 2.
 
 	return H_w_halted, Hpos
@@ -206,7 +251,7 @@ def get_alive_halted_mass(model_intervals, timepoints):
 	for time in timepoints:
 
 		Q = 0
-		for r in range(MAX_RUNS + 1):
+		for r in range(MAX_CELL_CYCLES + 1):
 				Q += Qr(mu0, sigma0, sigmav, delta, lambda_val, time, r, alpha)
 		if time == 0:
 			haltedMass = Q*halted
@@ -217,4 +262,36 @@ def get_alive_halted_mass(model_intervals, timepoints):
 
 	return mass_dic
 
+
+
+def compute_dna_content_s(config, tp):
+	"""Computes the expected DNA content for each timepoint defined in H.
+
+	G1 = copy number of 1
+	G2M = copy number of 2
+	S = linear interpolation based on progression through S
+
+	Can be multiplied against H to scale H appropriately by copy number.
+	"""
+
+	model_intervals = config.intervals_wt1
+	branch_phase_mapping = config.get_branch_phase_mapping()
+
+	parameters, relations, initial_timepoints, top_timepoints, bottom_timepoints, _ = model_intervals
+
+	if len(parameters) == 8:
+		mu0, lambda_val, delta, sigma0, sigmav, alpha, beta, halted = parameters
+	else:
+		mu0, lambda_val, delta, sigma0, sigmav, alpha, beta, gamma1, gamma2, halted = parameters	
+
+	c = 0
+	s_start = lambda_val*c + lambda_val*gamma1
+	s_end = lambda_val*c + lambda_val*gamma2
+
+	s_len = s_end - s_start
+
+	prop_through_s = (tp-s_start) / s_len
+
+	# Add one to change range from 1-2
+	return prop_through_s + 1.
 