1006b1f204c05c993c1ab066db24d5d971b8cb91
Trung Tran <trung.tran@duke.edu>
2024-06-14 13:00:01 -0400

    Add plot for number of genes above or below threshold for change in PTR in copy # analysis

--- src/Figure3_Copy_Correction.py
+++ src/Figure3_Copy_Correction.py
@@ -10,17 +10,52 @@ class Figure3CopyCorrection(object):
 
 	def __init__(self):
 
+		# Chromatin
+
 		self.chrom_ptrs_rep1 = pd.read_csv('output/copy_correction/chromatin/ptrs_rep1_shared.csv')\
 			.set_index('orf_name')
 		self.chrom_ptrs_rep2 = pd.read_csv('output/copy_correction/chromatin/ptrs_rep2_shared.csv')\
 			.set_index('orf_name')
 		self.mean_chrom_ptrs = (self.chrom_ptrs_rep1 + self.chrom_ptrs_rep2) / 2.
+		self.mean_chrom_ptrs['log_ratio'] = \
+			np.log2(self.mean_chrom_ptrs['normalized_corrected_ptr'] / self.mean_chrom_ptrs['normalized_raw_ptr']+1)
+		self.delta_chrom = 0.005
+
+		# Gene expression
 
 		self.ge_ptrs_rep1 = pd.read_csv('output/copy_correction/gene_expression/expression_ptr_comparison_rep1_shared.csv')\
 			.set_index('orf_name')
 		self.ge_ptrs_rep2 = pd.read_csv('output/copy_correction/gene_expression/expression_ptr_comparison_rep2_shared.csv')\
 			.set_index('orf_name')
 		self.mean_ge_ptrs = (self.ge_ptrs_rep1 + self.ge_ptrs_rep2) / 2.
+		self.mean_ge_ptrs['log_ratio'] = \
+			np.log2(self.mean_ge_ptrs['corrected_ptr'] / self.mean_ge_ptrs['raw_ptr']+1)
+		self.delta_ge = 0.005
+
+		def add_repl_timing_group_id(ptrs_data_df):
+			"""Add group ids for the replication timing, will be useful for box plots"""
+
+			from src.helpers import get_quantile_values
+
+			segments, qvals, lens = get_quantile_values(ptrs_data_df.replication_time, 
+				q=[0.25, 0.5, 0.75])
+
+			early_genes, early_mid_genes, mid_late_genes, late_genes = segments
+
+			ptrs_data_df.loc[early_genes.index, 'group_id'] = 0
+			ptrs_data_df.loc[early_mid_genes.index, 'group_id'] = 1
+			ptrs_data_df.loc[mid_late_genes.index, 'group_id'] = 2
+			ptrs_data_df.loc[late_genes.index, 'group_id'] = 3
+
+			ptrs_data_df.loc[early_genes.index, 'group_name'] = 'Early'
+			ptrs_data_df.loc[early_mid_genes.index, 'group_name'] = 'Early-Mid'
+			ptrs_data_df.loc[mid_late_genes.index, 'group_name'] = 'Mid-Late'
+			ptrs_data_df.loc[late_genes.index, 'group_name'] = 'Late'
+
+		# Add group information for box plotting
+		add_repl_timing_group_id(self.mean_ge_ptrs)
+		add_repl_timing_group_id(self.mean_chrom_ptrs)
+
 
 	def plot_chrom_ptr_correction(self):
 		
@@ -77,30 +112,11 @@ class Figure3CopyCorrection(object):
 		plt.ylabel("Copy-number-corrected expression PTR")
 
 	def plot_chrom_ptr_rep_quantiles(self):
-		from src.helpers import get_quantile_values
 
 		chrom_ptrs = self.mean_chrom_ptrs.copy()
 		n = len(chrom_ptrs)
 
-		log_ratio = np.log2(chrom_ptrs['normalized_corrected_ptr'] / chrom_ptrs['normalized_raw_ptr']+1)
-		chrom_ptrs['log_ratio'] = log_ratio
-
-		segments, qvals, lens = get_quantile_values(chrom_ptrs.replication_time, 
-			q=[0.25, 0.5, 0.75])
-
-		early_genes, early_mid_genes, mid_late_genes, late_genes = segments
-
 		from src.boxplot import BoxPlotPlotter
-
-		chrom_ptrs.loc[early_genes.index, 'group_id'] = 0
-		chrom_ptrs.loc[early_mid_genes.index, 'group_id'] = 1
-		chrom_ptrs.loc[mid_late_genes.index, 'group_id'] = 2
-		chrom_ptrs.loc[late_genes.index, 'group_id'] = 3
-
-		chrom_ptrs.loc[early_genes.index, 'group_name'] = 'Early'
-		chrom_ptrs.loc[early_mid_genes.index, 'group_name'] = 'Early-Mid'
-		chrom_ptrs.loc[mid_late_genes.index, 'group_name'] = 'Mid-Late'
-		chrom_ptrs.loc[late_genes.index, 'group_name'] = 'Late'
 
 		box_plotter = BoxPlotPlotter()
 		box_plotter.set_data([chrom_ptrs.sort_values('replication_time'),
@@ -123,37 +139,54 @@ class Figure3CopyCorrection(object):
 		plt.xlabel("Replication timing")
 		plt.title(f"Chromatin copy correction\nchange in PTR, n={n}", 
 				  fontsize=FiguresConfig.FIG_TITLE_FONTSIZE, pad=10)
-		plt.axhline(1, c='black', lw=0.5, ls='dotted', zorder=0)
+		plt.axhline(1, c='black', lw=0.5, ls='solid', zorder=0)
 
 		plt.ylim(-0.07, 0.07)
 		plt.ylim(0.95, 1.05)
 
+		plt.axhline(1-self.delta_chrom, c='#ddd', lw=0.7, ls='solid')
+		plt.axhline(1+self.delta_chrom, c='#ddd', lw=0.7, ls='solid')
+
+	def plot_chrom_ptr_counts(self):
+		from src.Figure3_Copy_Correction import rep_quantile_colors
+
+		plt.figure(figsize=FiguresConfig.FIGSIZE_SHORT)
+
+		ptrs = self.mean_chrom_ptrs
+
+		group_names = ['Early', 'Early-Mid', 'Mid-Late', 'Late']
+
+		for i in range(len(group_names)):
+			group_name = group_names[i]
+			current_ptrs = ptrs[ptrs.group_name == group_name]
+			color = rep_quantile_colors()[i]
+			
+			from src.plot_helpers import adjust_lightness_saturation
+			neg_color = adjust_lightness_saturation(color, 0.6, 0.75)
+			
+			plt.bar(i, -len(current_ptrs[current_ptrs.log_ratio < 1-self.delta_chrom]), color=neg_color, 
+					width=0.3)
+			plt.bar(i, len(current_ptrs[current_ptrs.log_ratio > 1+self.delta_chrom]), color=color, 
+					width=0.3)
+
+		plt.xticks(np.arange(len(group_names)), group_names)
+		yticks = np.arange(-1000, 800, 200)
+		ytick_labels = [str(np.abs(y)) for y in yticks]
+		plt.yticks(yticks, ytick_labels)
+		plt.ylim(-700, 300)
+		plt.xlim(-0.5, 3.5)
+		plt.axhline(0, c='black', lw=1, zorder=0)
+		plt.ylabel("# of genes w/\nincreased or decreased PTR")
+		plt.title(f"Genes with increased/decreased \nChromatin PTR "
+				  f"(±{self.delta_chrom}) after correction",
+				 pad=10, fontsize=FiguresConfig.FIG_TITLE_FONTSIZE)
+
 
 	def plot_ge_ptr_rep_quantiles(self):
-		from src.helpers import get_quantile_values
-
-		chrom_ptrs = self.mean_ge_ptrs.copy()
-		n = len(chrom_ptrs)
-
-		log_ratio = np.log2(chrom_ptrs['corrected_ptr'] / chrom_ptrs['raw_ptr']+1)
-		chrom_ptrs['log_ratio'] = log_ratio
-
-		segments, qvals, lens = get_quantile_values(chrom_ptrs.replication_time, 
-			q=[0.25, 0.5, 0.75])
-
-		early_genes, early_mid_genes, mid_late_genes, late_genes = segments
-
 		from src.boxplot import BoxPlotPlotter
 
-		chrom_ptrs.loc[early_genes.index, 'group_id'] = 0
-		chrom_ptrs.loc[early_mid_genes.index, 'group_id'] = 1
-		chrom_ptrs.loc[mid_late_genes.index, 'group_id'] = 2
-		chrom_ptrs.loc[late_genes.index, 'group_id'] = 3
-
-		chrom_ptrs.loc[early_genes.index, 'group_name'] = 'Early'
-		chrom_ptrs.loc[early_mid_genes.index, 'group_name'] = 'Early-Mid'
-		chrom_ptrs.loc[mid_late_genes.index, 'group_name'] = 'Mid-Late'
-		chrom_ptrs.loc[late_genes.index, 'group_name'] = 'Late'
+		chrom_ptrs = self.mean_chrom_ptrs
+		n = len(chrom_ptrs)
 
 		box_plotter = BoxPlotPlotter()
 		box_plotter.set_data([chrom_ptrs.sort_values('replication_time'),
@@ -173,10 +206,48 @@ class Figure3CopyCorrection(object):
 		plt.xlabel("Replication timing")
 		plt.title(f"Gene expression copy correction\nchange in PTR, n={n}", 
 				  fontsize=FiguresConfig.FIG_TITLE_FONTSIZE, pad=10)
-		plt.axhline(1, c='black', lw=0.5, ls='dotted', zorder=0)
+		plt.axhline(1, c='black', lw=0.5, ls='solid', zorder=0)
 
 		plt.ylim(-0.07, 0.07)
-		plt.ylim(0.97, 1.05)
+		plt.ylim(0.96, 1.05)
+		plt.axhline(1-self.delta_ge, c='#ddd', lw=0.7, ls='solid')
+		plt.axhline(1+self.delta_ge, c='#ddd', lw=0.7, ls='solid')
+
+
+	def plot_gene_expression_ptr_counts(self):
+		from src.Figure3_Copy_Correction import rep_quantile_colors
+
+		plt.figure(figsize=FiguresConfig.FIGSIZE_SHORT)
+
+		ptrs = self.mean_ge_ptrs
+
+		group_names = ['Early', 'Early-Mid', 'Mid-Late', 'Late']
+
+		for i in range(len(group_names)):
+			group_name = group_names[i]
+			current_ptrs = ptrs[ptrs.group_name == group_name]
+			color = rep_quantile_colors()[i]
+			
+			from src.plot_helpers import adjust_lightness_saturation
+			neg_color = adjust_lightness_saturation(color, 0.6, 0.75)
+			
+			plt.bar(i, -len(current_ptrs[current_ptrs.log_ratio < 1-self.delta_ge]), color=neg_color, 
+					width=0.3)
+			plt.bar(i, len(current_ptrs[current_ptrs.log_ratio > 1+self.delta_ge]), color=color, 
+					width=0.3)
+
+		plt.xticks(np.arange(len(group_names)), group_names)
+		yticks = np.arange(-600, 800, 100)
+		ytick_labels = [str(np.abs(y)) for y in yticks]
+		plt.yticks(yticks, ytick_labels)
+		plt.ylim(-250, 650)
+		plt.xlim(-0.5, 3.5)
+		plt.axhline(0, c='black', lw=1, zorder=0)
+		plt.ylabel("# of genes w/\nincreased or decreased PTR")
+		plt.title(f"Genes with increased/decreased \nExpression PTR "
+				  f"(±{self.delta_ge}) after correction",
+				 pad=10, fontsize=FiguresConfig.FIG_TITLE_FONTSIZE)
+
 
 
 from src.plot_helpers import adjust_lightness_saturation
