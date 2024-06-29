453cceca076f65aba48b5a087e10270d8454c474
Trung Tran <trung.tran@duke.edu>
2024-06-28 20:19:06 -0400

    Move script

--- scripts/submit_shared_runs.sh
+++ scripts/submit_shared_runs.sh
@@ -2,10 +2,10 @@
 # Script to create jobs to create locus plots for all genes across all chromosomes
 
 # OUTDIR=output/deconvolve_sharedg1_0066_cc_2024_06_13
-OUTDIR=output/deconvolve_sharedg1_gopt_cc_2024_06_14
+OUTDIR=output/deconvolve_sharedg1_gopt_nocc_2024_06_25
 
 #ARGS="${OUTDIR} 0.0066 shared 1"
-ARGS="${OUTDIR} -1 shared 1"
+ARGS="${OUTDIR} -1 shared 0"
 
 # There are 5774 in total, so 5774 jobs. We will split them into 1000 batch jobs (because of a limitation on the size
 # of the array on the slurm computing cluster. Therefore we also need a batch argument in ARGS)
