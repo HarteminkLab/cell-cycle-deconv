ed466360329eb9512aa372d6b443b49dc933d65c
Trung Tran <trung.tran@duke.edu>
2024-07-23 11:00:13 -0400

    Setup origin and gene deconvolution of 11x11 bins

--- scripts/archive/submit_deconv_combined_gamma006.sh
+++ scripts/archive/submit_deconv_combined_gamma006.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Script to create jobs to create locus plots for all genes across all chromosomes
 
-OUTDIR=output/deconvolve_combined_g006_2024_03_08_dg1_fix
+OUTDIR=output/deconvolve_combined_g006_2024_07_23
 
 # There are 5774 in total, so 5774 jobs. We will split them into 1000 batch jobs (because of a limitation on the size
 # of the array on the slurm computing cluster. Therefore we also need a batch argument in ARGS)
