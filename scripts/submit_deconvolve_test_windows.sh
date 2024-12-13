feb4bd12972592200e4770db9b3a6435813d7742
Trung Tran <trung.tran@duke.edu>
2024-12-13 16:25:54 -0500

    Create set of test windows for early and late replication regions and origins for copy correction testing

--- scripts/submit_deconvolve_test_windows.sh
+++ scripts/submit_deconvolve_test_windows.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # Script to create jobs to create locus plots for all genes across all chromosomes
 
-OUTDIR=output/deconvolved_copy_correction_3sites_offset01
+OUTDIR=output/deconvolved_copy_correction_3sites_offset1_2024_12_13
 ARGS="${OUTDIR} 1"
-sbatch -a 0-3%3 -D ./slurm-logs/ --job-name="cc" -p compsci --export="PYFILE=src/deconvolve_test_windows.py,ARGS=$ARGS,BATCH=0" scripts/cpu_genomicarray_job.sh
+sbatch -a 0-8%3 -D ./slurm-logs/ --job-name="cc" -p compsci --export="PYFILE=src/deconvolve_test_windows.py,ARGS=$ARGS,BATCH=0" scripts/cpu_genomicarray_job.sh
+
+OUTDIR=output/deconvolved_no_copy_correction_3sites_offset1_2024_12_13
+ARGS="${OUTDIR} 0"
+sbatch -a 0-8%3 -D ./slurm-logs/ --job-name="no_cc" -p compsci --export="PYFILE=src/deconvolve_test_windows.py,ARGS=$ARGS,BATCH=0" scripts/cpu_genomicarray_job.sh
