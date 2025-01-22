Fix computation of dg1 timepoints, create plots and algorithm for searchign for possible gamma2 values

src/RealDataReplication.py
@@ -434,6 +434,12 @@ class RealDataReplicationDeconvolution():
 		plt.title("Example replication curves", pad=11)
 
 
+	def update_parameters(self, updated_parameters):
+		self.config.params_dic.update(updated_parameters)
+		self.config.update_timepoints()
+		self.config.calculate_H()
+		self.H = self.config.H
+
 # def threshold_selection(dat, threshold_selection, fill=1.,
 # 	renormalize=False):
 # 	new_dat = dat.copy()
@@ -481,6 +487,9 @@ def plot_histogram_occupancies_G(config, G):
 
 def plot_heatmaps(N, F, H, B, G, column_names, full_column_names):
 
+	RdBu_cmap = plt.cm.RdBu_r
+	RdBu_cmap.set_bad('#aaaaaa')  # Set the color for NaN values
+
 	inv_B = np.linalg.inv(B)
 
 	HF = H@F
@@ -507,36 +516,36 @@ def plot_heatmaps(N, F, H, B, G, column_names, full_column_names):
 	plt.figure(figsize=(13, 11))
 
 	plt.subplot(6, 1, 1)
-	plt.imshow(F, cmap='RdBu_r', vmin=0, vmax=2, interpolation='none', aspect='auto')
+	plt.imshow(F, cmap=RdBu_cmap, vmin=0, vmax=2, interpolation='none', aspect='auto')
 	plt.xticks([])
 	plt.colorbar()
 	plt.title("$F$")
 
 	plt.subplot(6, 1, 2)
-	plt.imshow(HF, cmap='RdBu_r', vmin=0, vmax=2, interpolation='none', aspect='auto')
+	plt.imshow(HF, cmap=RdBu_cmap, vmin=0, vmax=2, interpolation='none', aspect='auto')
 	plt.colorbar()
 	plt.xticks([])
 	plt.title("$HF$")
 
 	plt.subplot(6, 1, 3)
-	plt.imshow(Ninv_G_B_inv, cmap='RdBu_r', vmin=0, vmax=2, interpolation='none', aspect='auto')
+	plt.imshow(Ninv_G_B_inv, cmap=RdBu_cmap, vmin=0, vmax=2, interpolation='none', aspect='auto')
 	plt.colorbar()
 	plt.title("$(N^{-1})G(B^{-1})$")
 
 	plt.subplot(6, 1, 4)
-	plt.imshow(predicted_G, cmap='RdBu_r', vmin=0, vmax=2, interpolation='none', aspect='auto')
+	plt.imshow(predicted_G, cmap=RdBu_cmap, vmin=0, vmax=2, interpolation='none', aspect='auto')
 	plt.xticks([])
 	plt.colorbar()
 	plt.title("Predicted G: $NHFB$")
 
 	plt.subplot(6, 1, 5)
-	plt.imshow(G, cmap='RdBu_r', vmin=0, vmax=2, interpolation='none', aspect='auto')
+	plt.imshow(G, cmap=RdBu_cmap, vmin=0, vmax=2, interpolation='none', aspect='auto')
 	plt.colorbar()
 	plt.xticks([])
 	plt.title("$G$")
 
 	plt.subplot(6, 1, 6)
-	plt.imshow(residual_diff, vmin=-1, vmax=1, cmap='RdBu_r', interpolation='none', aspect='auto')
+	plt.imshow(residual_diff, vmin=-1, vmax=1, cmap=RdBu_cmap, interpolation='none', aspect='auto')
 	plt.colorbar()
 	plt.title("$NHFB - G$")
 	plt.subplots_adjust(hspace=0.5, top=0.9)

src/deconvolution_solver.py
@@ -5,14 +5,27 @@ from src.helpers import get_wavelet_kernel
 
 class DeconvolutionSolver(object):
 
-	def __init__(self, config, g, H, gamma, N, f_replication,
-	 	b, padding_type='both'):
+	def __init__(self, config, g, H, gamma, N=None, f_replication=None,
+	 	b=None, padding_type='both'):
+
+		n, m = H.shape
+
+		if N is None:
+			N = np.eye(n)
+
+		if f_replication is None:
+			f_replication = np.ones(m)
+
+		if b is None:
+			b = 1
+
 		self.config = config
 		self.g = g
 		self.H = H
 		self.N = N
 		self.f_replication = f_replication
 		self.b = b
+
 		self.gamma = gamma
 		self.padding_type = padding_type
 		

src/model.py
@@ -23,7 +23,8 @@ class Model:
 		g (list of float): Measured time series population data.
 	"""
 	
-	def __init__(self, config, gene_or_orfname, gamma=0.0, for_chromatin_deconv=False):
+	def __init__(self, config, gene_or_orfname, gamma=0.0, for_chromatin_deconv=False,
+		expression_data=None):
 
 		if gene_or_orfname is None:
 			self.orf_name = None
@@ -35,17 +36,14 @@ class Model:
 		self.gamma = gamma
 
 		if not for_chromatin_deconv:
-			if self.config.wt1_df is not None:
-				g1 = self.config.wt1_df.loc[self.orf_name].values
-				self.g1 = g1
-				self.g = g1
+			g1 = expression_data.loc[self.orf_name].values
+			self.g1 = g1
+			self.g = g1
 
-		self.initial_phase_map, self.top_phase_map, self.bottom_phase_map = config.intervals_wt1[-1]
+		H1 = config.H
 
-		calcH_function = config.calcH_function
-		H1, self.Hpos = calcH_function(config.intervals_wt1, config.WT1_TIMEPOINTS)
-
-		if self.config.has_two_replicates:
+		# todo: testing refactor of config for single replicate
+		if False:
 
 			if not for_chromatin_deconv:
 				# Load g2
@@ -62,19 +60,10 @@ class Model:
 
 				self.g = np.concatenate((g1, g2))
 
-			H2, _ = calcH_function(config.intervals_wt2, config.WT2_TIMEPOINTS)
+			H2, _ = calcH_function(config.intervals_wt2, config.timepoints)
 			self.H = np.concatenate((H1, H2))
 
 		else:
-
-			if not for_chromatin_deconv:
-				# Just correct g1, g
-				if config.copy_correction is not None:
-					print_fl("Applying copy number correction")
-					copy_correction_vector = config.copy_correction.loc[self.orf_name]
-					self.g1 = self.g1 * copy_correction_vector
-					self.g = self.g1
-
 			self.H = H1
 
 	def deconvolve_find_optimal_gamma(self, silence=True):
@@ -114,7 +103,7 @@ class Model:
 		g1 = self.g1
 		predicted_g = self.pred_g
 
-		if self.config.has_two_replicates:
+		if False: #self.config.has_two_replicates:
 			g2 = self.g2
 			g = np.concatenate([g1, g2])
 
@@ -138,31 +127,26 @@ class Model:
 		if H is None:
 			H = self.H
 
-		rg1_cols = self.config.phase_columns['RG1']
-		cg1_cols = self.config.phase_columns['CG1']
-		dg1_cols = self.config.phase_columns['DG1']
+		rg1_cols = self.config.get_Hpositions_for_phase('RG1')
+		cg1_cols = self.config.get_Hpositions_for_phase('CG1')
+		dg1_cols = self.config.get_Hpositions_for_phase('DG1')
 
 		H_cols = np.array([H.shape[1]-1])
 
-		if 'postG1' in self.config.phase_columns:
-			post_g1_cols = self.config.phase_columns['postG1']
-			phases = ['H', 'RG1', 'CG1', 'DG1', 'postG1']
-			cols_list = [H_cols, rg1_cols, cg1_cols, dg1_cols, post_g1_cols]
-		else:
-			s_cols = self.config.phase_columns['S']
-			g2m_cols = self.config.phase_columns['G2M']
-			phases = ['H', 'RG1', 'CG1', 'DG1', 'S', 'G2M']
-			cols_list = [H_cols, rg1_cols, cg1_cols, dg1_cols, s_cols, g2m_cols]
+		s_cols = self.config.get_Hpositions_for_phase('S')
+		g2m_cols = self.config.get_Hpositions_for_phase('G2M')
+		phases = ['H', 'RG1', 'CG1', 'DG1', 'S', 'G2M']
+		cols_list = [H_cols, rg1_cols, cg1_cols, dg1_cols, s_cols, g2m_cols]
 
 		plt.figure(figsize=(16, 3))
 		plt.subplot(1, 2, 1)
 
 		plt.imshow(H, vmax=H[:, :-1].max(), aspect='auto', cmap='Reds',
-				  extent=[0, H.shape[1], self.config.WT1_TIMEPOINTS[-1], 0])
+				  extent=[0, H.shape[1], self.config.timepoints[-1], 0])
 
 		plt.subplot(1, 2, 2)
 
-		x = self.config.WT1_TIMEPOINTS
+		x = self.config.timepoints
 
 		prev = np.zeros(len(x))
 			
@@ -196,8 +180,8 @@ class Model:
 
 		# -----------------
 
-		timepoints1 = self.config.WT1_TIMEPOINTS
-		timepoints2 = self.config.WT2_TIMEPOINTS
+		timepoints1 = self.config.timepoints
+		timepoints2 = self.config.timepoints
 
 		if timepoints2 is not None:
 			xlims = timepoints1[0], max(timepoints1[-1], timepoints2[-1])
@@ -219,7 +203,7 @@ class Model:
 		# -----------------
 
 		if g2 is not None:
-			timepoints2 = self.config.WT2_TIMEPOINTS
+			timepoints2 = self.config.timepoints
 			ax4.plot(timepoints2, g2, color=self.color_for_key('raw'), lw=4)
 			ax4.plot(timepoints2, predicted_g2, color=self.color_for_key('fit'), lw=4)
 			# ax4.set_yscale('log')
@@ -228,33 +212,40 @@ class Model:
 
 		def _plot_branch(ax, branch, ylim, start_offset=0, linestyle='solid'):
 			"""Plot the branch coloring the individual phases within the branch"""
-			phase_tp_idx_list = self.config.get_timepoints_phases_Hpositions_for_branch(branch)
-
+			phase_tp_idx_list = self.config.branch_Hpos_df.loc[branch]
 
 			offset = 0
-			if start_offset:
-				offset = start_offset-phase_tp_idx_list[0][1].values[0]
+			#if start_offset:
+				#offset = start_offset-phase_tp_idx_list[0][1].values[0]
 
-			for phase, timepoints, indices in phase_tp_idx_list:
-				ax.plot(timepoints+offset, f[indices], color=self.color_for_key(phase), 
+			phases = phase_tp_idx_list.index.values
+			for phase in phases:
+
+				branch = 'i'
+				branch_tps = phase_tp_idx_list.loc[phase].timepoint_start
+				indices = phase_tp_idx_list.loc[phase].Hpos
+
+				ax.plot(branch_tps+offset, f[indices], color=self.color_for_key(phase), 
 					lw=5, linestyle=linestyle)
 
 			ax.set_ylim(ylim[0], ylim[1])
 
-			# return timepoints in case we want to append more branches on to the plot
-			return timepoints.values + offset
+			return branch_tps.values + offset
 
 		# ------------------
 
 		# Set the ylim appropriate to the values in f
-		diff = np.max(f) - np.min(f)
-		ylim = np.min(f)-diff*0.1, np.min(f)+diff*1.1
+		f_values_for_lim = f[:-1]
+		diff = np.max(f_values_for_lim) - np.min(f_values_for_lim)
+		ylim = np.min(f_values_for_lim)-diff*0.1, np.min(f_values_for_lim)+diff*1.1
 
 		# Halted cells are the last element in f
 		halted_f = f[len(f)-1]
 		ax1.scatter(-1, halted_f, color='gray', s=50, marker='H')
 
-		for phase, indices in self.config.phase_columns.items():
+		phases = ['RG1', 'CG1', 'DG1', 'postG1']
+		for phase in phases:
+			indices = self.config.get_Hpositions_for_phase(phase)
 			plot_f_values = f[indices]
 			ax1.plot(indices, plot_f_values, color=self.color_for_key(phase), lw=5)
 		ax1.set_ylim(*ylim)
@@ -291,8 +282,8 @@ class Model:
 
 		title = f"{self.gene_name} / {self.orf_name}, gamma={self.gamma:.4f}\nrn={self.rn:.4f}, sn={self.sn:.2f}"
 
-		if self.config.name is not None:
-			title = f"{self.config.name}\n" + title
+		#if self.config.name is not None:
+		#	title = f"{self.config.name}\n" + title
 
 		plt.suptitle(title, fontsize=23)
 		return fig

src/rg1_refactor.py
@@ -40,6 +40,7 @@ class RG1Model(object):
 		mu0 = self.params_dic['mu0']
 		lambda_val = self.params_dic['lambda']
 		gamma1 = self.params_dic['gamma1']
+		delta = self.params_dic['delta']
 		start_of_s = gamma1*lambda_val
 
 		# for now, let's set alpha to 0
@@ -47,11 +48,21 @@ class RG1Model(object):
 
 		rg1_time_span = mu0, start_of_s
 		cg1_time_span = -alpha, start_of_s
+		dg1_time_span = -delta-alpha, start_of_s
 		postg1_time_span = start_of_s, lambda_val-alpha
 
 		rg1_timepoints = np.linspace(rg1_time_span[0], rg1_time_span[1], G1_NUM_TPS+1)
 		cg1_timepoints = np.linspace(cg1_time_span[0], cg1_time_span[1], G1_NUM_TPS+1)
+		dg1_timepoints = np.linspace(dg1_time_span[0], dg1_time_span[1], G1_NUM_TPS+1)
 		postg1_timepoints = np.linspace(postg1_time_span[0], postg1_time_span[1], POSTG1_NUM_TPS+1)
+
+		Hpos = np.concatenate([
+					range(0, G1_NUM_TPS), # RG1
+					range(G1_NUM_TPS, G1_NUM_TPS*2), # CG1 and DG1
+					range(G1_NUM_TPS, G1_NUM_TPS*2), # CG1 and DG1
+					range(G1_NUM_TPS*2, G1_NUM_TPS*2+POSTG1_NUM_TPS), # CG1 and DG1
+					[G1_NUM_TPS*2+POSTG1_NUM_TPS], # Halted
+				])
 
 		# Create the dataframe that maps the H positions directly to the the timepoints
 		timepoints_dataframe = pd.DataFrame({
@@ -61,25 +72,38 @@ class RG1Model(object):
 			# these timepoints correspond
 			'timepoint_start': np.concatenate([rg1_timepoints[0:-1], 
 				cg1_timepoints[0:-1], 
-				postg1_timepoints[0:-1]]),
+				dg1_timepoints[0:-1], 
+				postg1_timepoints[0:-1], [0]]),
 
 			'timepoint_end': np.concatenate([rg1_timepoints[1:], 
 				cg1_timepoints[1:], 
-				postg1_timepoints[1:]]),
+				dg1_timepoints[1:], 
+				postg1_timepoints[1:], [0]],),
 
 			'phase': np.concatenate([
 				np.repeat('RG1', G1_NUM_TPS),
 				np.repeat('CG1', G1_NUM_TPS),
+				np.repeat('DG1', G1_NUM_TPS),
 				np.repeat('postG1', POSTG1_NUM_TPS),
+				np.repeat('Halted', 1),
 				])
+			,
+			'Hpos':
+				np.concatenate([
+					range(0, G1_NUM_TPS), # RG1
+					range(G1_NUM_TPS, G1_NUM_TPS*2), # CG1
+					range(G1_NUM_TPS, G1_NUM_TPS*2), # DG1 (same indices)
+					range(G1_NUM_TPS*2, G1_NUM_TPS*2+POSTG1_NUM_TPS), # Post G1
+					[G1_NUM_TPS*2+POSTG1_NUM_TPS], # Halted
+				]),
 			})
-		timepoints_dataframe.index.name = 'Hpos'
-		self.timepoints_df = timepoints_dataframe.reset_index().set_index('phase')
+		
+		self.timepoints_df = timepoints_dataframe.set_index('phase')
 
 		self.branch_phase_mapping = {
 			'i': ['RG1', 'postG1'],
 			't': ['CG1', 'postG1'],
-			'b': ['CG1', 'postG1'],
+			'b': ['DG1', 'postG1'],
 		}
 
 		# Create the dataframe that contains the per branch timepoints and
@@ -117,6 +141,7 @@ class RG1Model(object):
 		elif phase in ['S', 'G2M']:
 
 			s_indices, g2m_indices = self.get_s_g2m_indices()
+
 			if phase == 'S':
 				return s_indices
 			elif phase == 'G2M':
@@ -128,7 +153,19 @@ class RG1Model(object):
 		return self.branch_Hpos_df.loc[branch].Hpos.values
 		
 	def get_timepoints_for_phase(self, phase):
-		return self.timepoints_df.loc[phase].timepoint_start.values
+
+		if phase in ['S', 'G2M']:
+
+			s_indices = self.get_Hpositions_for_phase('S')
+			postG1_rows = self.timepoints_df.loc['postG1']
+
+			if phase == 'S':
+				return postG1_rows[postG1_rows.Hpos <= s_indices[-1]].timepoint_start.values
+			else:
+				return postG1_rows[postG1_rows.Hpos > s_indices[-1]].timepoint_start.values
+
+		else:
+			return self.timepoints_df.loc[phase].timepoint_start.values
 
 	def get_timepoints_for_branch(self, branch):
 		return self.branch_Hpos_df.loc[branch].timepoint_start.values
@@ -235,6 +272,8 @@ class RG1Model(object):
 				phase_timepoint_starts = self.branch_Hpos_df.loc[branch].loc[phase].timepoint_start.values
 				phase_timepoint_ends = self.branch_Hpos_df.loc[branch].loc[phase].timepoint_end.values
 				phase_timepoints = np.concatenate([phase_timepoint_starts, phase_timepoint_ends[-1:]])
+
+				print(phase, phase_timepoint_starts)
 				timepoints_for_branch.append(phase_timepoints)
 
 			return timepoints_for_branch