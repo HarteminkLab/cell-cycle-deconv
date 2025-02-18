Fix bug in normalization, adjust chromatin deconvolution scaling to center on mean 1, to match copy correction procedure. Trial kappa and gamma values are functional. Now testing signal to noise idea for daughter-specific changes.

src/RealDataReplication.py
@@ -102,12 +102,49 @@ class RealDataReplicationDeconvolution():
 		self.rn = result.iterative_update_rns[-1]
 		self.B = result.Bs[-1]
 
-		return result
+		self.F_df = pd.DataFrame(self.F, columns=self.masked_G_df.columns,
+		    index=range(self.F.shape[0]))
+		self.b_df = pd.DataFrame(np.diag(self.B), index=self.masked_G_df.columns,
+		                   columns=['b']).T
 
 	def compute_rn(self):
 		N, H, F, B = self.N, self.H, self.F, self.B
 		G = self.G
 		return compute_rn(N, H, F, B, G)
+
+
+	def save_to_disk(self, out_directory):
+		"""Save the deconvolved replication profiles to disk"""
+		from src.utils import mkdirs_safe
+
+		self.save_dir = out_directory
+
+		mkdirs_safe([out_directory])
+
+		N_save_path = f'{self.save_dir}/rep{self.replicate}_chr{self.chrom}_N.npy'
+		B_save_path = f'{self.save_dir}/rep{self.replicate}_chr{self.chrom}_B.csv'
+		F_save_path = f'{self.save_dir}/rep{self.replicate}_chr{self.chrom}_F.csv'
+		H_save_path = f'{self.save_dir}/rep{self.replicate}_chr{self.chrom}_H.npy'
+
+		parameters_save_path = f'{self.save_dir}/rep{self.replicate}_chr{self.chrom}_parameters.csv'
+		fig_path = f'{self.save_dir}/rep{self.replicate}_chr{self.chrom}.png'
+
+		np.save(N_save_path, self.N)
+		np.save(H_save_path, self.config.H)
+		self.F_df.to_csv(F_save_path)
+		self.b_df.to_csv(B_save_path)
+		pd.DataFrame(self.config.params_dic, index=[0]).to_csv(parameters_save_path)
+
+		fig = self.plot_heatmaps()
+		plt.savefig(fig_path, dpi=200)
+		plt.close(fig)
+
+		print_fl(f"Saved to: {N_save_path}")
+		print_fl(f"Saved to: {B_save_path}")
+		print_fl(f"Saved to: {H_save_path}")
+		print_fl(f"Saved to: {F_save_path}")
+		print_fl(f"Saved to: {parameters_save_path}")
+		print_fl(f"Saved to: {fig_path}")
 
 
 	def update_N_B(self, N, H, G, B, F):
@@ -134,6 +171,7 @@ class RealDataReplicationDeconvolution():
 			full_column_names=self.G_df.columns)
 		plt.suptitle(f"Replication {self.replicate}"
 			f" deconvolution,\nChromosome {self.chrom}")
+
 		return fig
 
 	def plot_B(self):
@@ -496,22 +534,36 @@ def compute_N(config, plot=False):
 def read_n_fr_b(chrom, deconv_span):
 	from src.mnase_10kb_loader import get_bin_for_position
 
-	N = np.load('data/copy_correction/N_combined.npy')
-	B_df = pd.read_csv(f'data/copy_correction/B_chr{chrom}_combined.csv').set_index('start')
-	Fr_df = pd.read_csv(f'data/copy_correction/F_chr{chrom}_combined.csv')
-	B_df.columns = B_df.columns.astype(int)
+	# Trial replication profile from 2/17/25 run
+
+	print_fl(f"Loading trial replication profile, 2/17/25")
+
+	replicate = 1
+
+	directory = 'output/replication_profiles_trial'
+
+	N = np.load(f'{directory}/rep{replicate}_chr{chrom}_N.npy')
+	Fr_df = pd.read_csv(f'{directory}/rep{replicate}_chr{chrom}_F.csv')
+	B_df = pd.read_csv(f'{directory}/rep{replicate}_chr{chrom}_B.csv')
+
+	# Fix the column names
+	Fr_df = Fr_df[Fr_df.columns[1:]]
 	Fr_df.columns = Fr_df.columns.astype(int)
 
-	start_indices = B_df.index
+	B_df = B_df[B_df.columns[1:]]
+	B_df.columns = B_df.columns.astype(int)
+	B_df = B_df.T
+
+	start_indices = Fr_df.columns
 
 	mid_span = (deconv_span[0]+deconv_span[1])/2
+
 	bin_idx, start = get_bin_for_position(mid_span, start_indices)
 
-	# Load the b and f_replication for the span to be deconvolved.
-	b = B_df.loc[start].loc[start]
-	fr = Fr_df[start]
+	fr = Fr_df[start].values
+	b = B_df.loc[start].values[0]
 
-	return N, fr, b
+	return start, N, fr, b
 
 
 def read_no_copy_correction_n_fr_b(H):

src/chromatin_deconvolution_solver.py
@@ -57,7 +57,8 @@ class ChromatinDeconvolveSolver:
 		return predicted_G
 
 
-	def deconvolve_G_iteratively(self, gamma, verbose=False, verbose_progress=True):
+	def deconvolve_G_iteratively(self, gamma, verbose=False, verbose_progress=True,
+		kappa=1e-4):
 		"""Iteratively deconvolve columns of G, appears to be more accurate
 		as the optimization can strictly treat each problem independently"""
 
@@ -71,6 +72,7 @@ class ChromatinDeconvolveSolver:
 		running_sn = 0
 		running_rn = 0
 		self.gamma = gamma
+		self.kappa = kappa
 
 		eps_cutoff = 1e-3
 
@@ -89,15 +91,16 @@ class ChromatinDeconvolveSolver:
 
 				deconvolution_solver = DeconvolutionSolver(self.config, current_g, 
 					self.H, gamma=gamma, padding_type=self.padding_type,
-					N=self.N, f_replication=self.f_replication, b=self.b)
+					N=self.N, f_replication=self.f_replication, b=self.b,
+					kappa=kappa)
 
-				try:
-					deconvolution_solver.deconvolve()
-					current_f = deconvolution_solver.f
-					current_sn = deconvolution_solver.sn
-					current_rn = deconvolution_solver.rn
-				except cvxpy.error.SolverError:
-					continue
+				# try:
+				deconvolution_solver.deconvolve()
+				current_f = deconvolution_solver.f
+				current_sn = deconvolution_solver.sn
+				current_rn = deconvolution_solver.rn
+				# except cvxpy.error.SolverError:
+				# 	continue
 
 				deconvolved_f_value[:, i] = current_f
 
@@ -148,7 +151,7 @@ def plot_branches(chromatin_model, full_deconvolved_F):
 
 	num_imgs_per_branch = 12
 
-	fig, axs = plt.subplots(num_imgs_per_branch+1, 4, figsize=(11, 7))
+	fig, axs = plt.subplots(num_imgs_per_branch+1, 4, figsize=(5, 7))
 	axs = np.array(axs).T
 
 	def plot_branch_imgs(row_axs, t_indices):
@@ -158,7 +161,7 @@ def plot_branches(chromatin_model, full_deconvolved_F):
 			image_index = t_indices[int(index_in_t)]
 
 			ax = row_axs[plot_index]
-			ax.imshow(full_F_imgs[image_index], aspect='auto', cmap='magma_r', vmax=0.1,
+			ax.imshow(full_F_imgs[image_index], aspect='auto', cmap='magma_r', vmax=50,
 					  origin='lower')
 			ax.set_xticks([])
 			ax.set_yticks([])
@@ -166,16 +169,17 @@ def plot_branches(chromatin_model, full_deconvolved_F):
 			
 	def plot_difference(row_axs, t_indices, b_indices):
 
+		eps = 2
 		for plot_index, index_in_t in enumerate(np.linspace(0,
 			len(t_indices)-1, num_imgs_per_branch)):
 			
 			image_index_t = t_indices[int(index_in_t)]
 			image_index_b = b_indices[int(index_in_t)]
 
-			img_diff = full_F_imgs[image_index_t]-full_F_imgs[image_index_b]
+			img_diff = np.log2((full_F_imgs[image_index_b]+eps)/(full_F_imgs[image_index_t]+eps))
 			
 			ax = row_axs[plot_index]
-			ax.imshow(img_diff, aspect='auto', cmap='RdBu_r', vmin=-0.05, vmax=0.05,
+			ax.imshow(img_diff, aspect='auto', cmap='RdBu_r', vmin=-5, vmax=5,
 					  origin='lower')
 			ax.set_xticks([])
 			ax.set_yticks([])
@@ -202,27 +206,53 @@ def plot_branches(chromatin_model, full_deconvolved_F):
 	return fig
 
 
-def example_N_frep_b(config):
+def subset_select_highest_G_indices(G, num_examples=5):
+    G_max_df = pd.DataFrame({'max_value': G.max(axis=0), 
+        'index': np.arange(G.shape[1])})
+    highest_Gs = G_max_df.sort_values('max_value', 
+        ascending=False).head(num_examples)['index'].values
+    G_values = G[:, highest_Gs].copy()
+    return G_values, highest_Gs
+
+
+def dummy_N_frep_b(config):
+	"""Example N, f-replication and b for testing, serves as a stand-in for 
+	copy correction specifications. Will be replaced by actual replication timing
+	profile."""
 
 	n, m = config.H.shape
 
-	padding_type = 'both'
-
 	b = 1
 
+	# Example normalization term from chromosome 4
 	chr4_n = np.array([1.00442364, 1.00347336, 0.97773201, 0.8061213 , 0.66388052,
 		   0.84322967, 0.99955218, 1.00893152, 0.97884289, 0.85742082,
 		   0.75599297, 0.76982516, 0.84182678, 0.97059662, 0.99078551,
 		   0.91119956])
 
-	N = np.diag(chr4_n)
+	s_indices = config.get_Hpositions_for_phase('S')
+	repl_idx = s_indices[len(s_indices)//2]
 
-	# f_rep = np.array([1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1.,
-	#        1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1.,
-	#        1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1.,
-	#        1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1., 1.,
-	#        1., 1., 1., 1., 1., 1., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2., 2.,
-	#        2., 1.])
+	N = np.diag(chr4_n)
+	f_rep = np.ones(m)
+	f_rep[repl_idx:-1] =  2
+
+	return N, f_rep, b
+
+
+def dummy_no_copy_correction_N_frep_b(config):
+	"""Example N, f-replication and b for testing, serves as a stand-in for 
+	copy correction specifications. Will be replaced by actual replication timing
+	profile."""
+
+	n, m = config.H.shape
+
+	b = 1
+
+	# Example normalization term from chromosome 4
+	n_diag = np.ones(n)
+
+	N = np.diag(n_diag)
 	f_rep = np.ones(m)
 
 	return N, f_rep, b

src/chromatin_model.py
@@ -766,22 +766,14 @@ def normalize_bins_by_len(replicate, exact_bins, log=True, scaling_mat=None):
 		from src.preprocessing import load_scaling_mat
 		scaling_mat = load_scaling_mat(replicate)
 
-	# Normalize to a consistent read depth, this ensures that any window
-	# of any size will have the same read depth per basepair
-	#
-	# todo: examine larger windows and determine if this read depth is logical
-	#
-	def normalize_to_depth(G, target_depth=0.005):
-		total_bp_area = G.shape[1] * G.shape[2]
-		scale = (target_depth * total_bp_area)
-		return G / G.sum(axis=(1, 2))[:, None, None] * scale
-
-	normalized_exact_bins = normalize_to_depth(exact_bins)
+	normalized_exact_bins = exact_bins
 
 	# Normalization that matches
 	# the length distribution across all timepoints and replicates
 	if log: print_fl("Applying a normalization for length distribution")
 
+	# Normalize using the scaling matrix which is a (fragment length x timepoints)
+	# large
 	scaling_T = scaling_mat.T
 	scaling_T = scaling_T.values.reshape((scaling_T.shape[0], scaling_T.shape[1], 1))
 	normalized_bins = normalized_exact_bins * scaling_T
@@ -789,6 +781,43 @@ def normalize_bins_by_len(replicate, exact_bins, log=True, scaling_mat=None):
 	return normalized_bins
 
 
+
+def plot_raw(chromatin_model):
+
+	config = chromatin_model.config
+	G = chromatin_model.G
+	G_imgs = G.reshape((G.shape[0], 26, -1))
+	timepoints = config.timepoints
+
+	num_rows = len(timepoints)+1
+	num_cols = 1
+
+	fig, axs = plt.subplots(num_rows, num_cols, figsize=(2, 7))
+
+	from src.orf_plotter import load_default_orf_plotter
+	from src.sgd import read_nondubious_genes_dataset
+
+	chrom, mnase_span = chromatin_model.chr, chromatin_model.mnase_span
+
+	orf_plotter = load_default_orf_plotter()
+	orf_plotter.set_span_chrom(mnase_span, chrom)
+
+	orf_plotter.plot_orf_annotations(axs[0])
+
+	for i in range(1, num_rows):
+
+		ax = axs[i]
+
+		ax.imshow(G_imgs[i-1], origin='lower', aspect='auto', vmin=0, vmax=0.2,
+				  cmap='magma_r')
+		ax.set_xticks([])
+		ax.set_yticks([])
+
+
+	plt.suptitle("Predicted vs Raw data bins")
+	plt.subplots_adjust(top=0.95)
+
+	return fig
 
 def plot_prediction(chromatin_model, N, F, F_replicate, b):
 
@@ -819,21 +848,21 @@ def plot_prediction(chromatin_model, N, F, F_replicate, b):
 
 		ax_row = axs_rows[i]
 		ax = ax_row[0]
-		ax.imshow(predicted_G_imgs[i-1], origin='lower', aspect='auto', vmin=0, vmax=0.2,
+		ax.imshow(predicted_G_imgs[i-1], origin='lower', aspect='auto', vmin=0, vmax=50,
 				  cmap='magma_r')
 		ax.set_xticks([])
 		ax.set_yticks([])
 		ax.set_ylabel(f"{timepoints[i-1]}'")
 		
 		ax = ax_row[1]
-		ax.imshow(G_imgs[i-1], origin='lower', aspect='auto', vmin=0, vmax=0.2,
+		ax.imshow(G_imgs[i-1], origin='lower', aspect='auto', vmin=0, vmax=50,
 				  cmap='magma_r')
 		ax.set_xticks([])
 		ax.set_yticks([])
 
 		ax = ax_row[2]
 		ax.imshow(G_imgs[i-1]-predicted_G_imgs[i-1], origin='lower', aspect='auto',
-				  cmap='RdBu_r', vmin=-0.1, vmax=0.1)
+				  cmap='RdBu_r', vmin=-10, vmax=10)
 		ax.set_xticks([])
 		ax.set_yticks([])
 

src/config.py
@@ -338,8 +338,9 @@ class RG1Model(object):
 			color = color_for_key(phase)
 			mass = H[:, cols].sum(axis=1)
 
-			if phase in ['CG1', 'DG1']:
-				mass = mass/2.
+			if self.config_type == 'shared':
+				if phase in ['CG1', 'DG1']:
+					mass = mass/2.
 
 			y = prev+mass
 

src/helpers.py
@@ -189,13 +189,6 @@ def createF(Hpos, phaseMap):
 
 def get_wavelet_kernel(N, type="Symmlet", par=8):
 	wavelet_kernel = WavMat(MakeONFilter(type, par), N)
-
-	# Create weighting matrix
-	# weights = np.ones(N)
-
-	# D = np.diag(weights)
-
-	# return wavelet_kernel, D
 	return wavelet_kernel
 
 
@@ -767,12 +760,11 @@ def midpoints(arr):
 
 
 def get_level_based_weights(N, scale=2):
+	"""Weight the coefficients by the level of of the wavelet. This
+	discourages high-frequency coefficients. Exponentially increasing
+	weighting based on coefficient level."""
 	
-	# Working gene expression weights
 	weights = np.ones(N)
-
-	weights = weights*2
-
 	levels = int(np.log2(N))
 
 	for level in range(levels):
@@ -781,8 +773,6 @@ def get_level_based_weights(N, scale=2):
 		weight = scale**(level+1)
 		weights[start_index:end_index] = weight
 	
-	# weights[N//4:] = 1024
-
 	return weights
 
 