26fdd05514cbb5b841952ffce39b772f875456c1
Trung Tran <trung.tran@duke.edu>
2025-02-19 10:52:46 -0500

    Compute chromatin entropy for all fragment lengths, preliminary test to quantify disorganization in DSE1. Next compare to intergenic "control" regions

---
+++ src/chromatin_entropy.py
@@ -0,0 +1,72 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from scipy.signal import convolve2d
+
+
+def select_sub_img_mid(f_imgs, window=500):
+    """Helper function to subselect a window of given size along the center
+    of the parent window.
+    
+    Input is an np array of f images (t, r, c).
+    Outputs (t, r, c'), where c' is length of the new window
+    """
+    midbin_idx = f_imgs.shape[2]//2
+    window = 500
+    win_2 = window//2
+    win_2_bins = win_2//10
+    bin_span = midbin_idx-win_2_bins, midbin_idx+win_2_bins
+    f_subset = f_imgs[:, :, bin_span[0]:bin_span[1]]
+    return f_subset
+
+
+# Functions to compute chromatin entropy
+def compute_occupancy_entropy(f_images, kernel=None):
+    """Compute the occupancy and entropy for a window, given a kernel
+    that can subselect read lengths, default is a kernel that selects all
+    fragment lengths along a column."""
+        
+    t, r, c = f_images.shape
+
+    if kernel is None:
+        kernel = np.ones((r, 1))
+        
+    kr, kc = kernel.shape
+    out_r = r - kr + 1
+    out_c = c - kc + 1
+        
+    # Pre-allocate output array
+    occupancy_result = np.zeros((t, out_r, out_c))
+    entropy_scores = np.zeros(t)
+    eps = 1e-5
+    
+    from src.helpers import calc_entropy
+
+    for i in range(t):
+        occupancy_result[i] = convolve2d(f_images[i], kernel, mode='valid')        
+
+        entropy_scores[i] = \
+            np.apply_along_axis(lambda row: calc_entropy(row+eps), axis=1, 
+            arr=occupancy_result[i])
+
+    return occupancy_result, entropy_scores
+
+
+def plot_occupancy_entropy_results(config, occupancy_result, entropy_result):
+
+	t_indices = config.get_Hpositions_for_branch('t')
+	b_indices = config.get_Hpositions_for_branch('b')
+
+	plt.figure(figsize=(9, 3))
+	plt.subplot(1, 2, 1)
+	plt.plot(occupancy_result[t_indices].mean(axis=2), label="Top")
+	plt.plot(occupancy_result[b_indices].mean(axis=2), label="Bottom")
+	plt.title("Nucleosome occupancy")
+	plt.legend()
+
+	plt.subplot(1, 2, 2)
+	plt.plot(entropy_result[t_indices], label="Top")
+	plt.plot(entropy_result[b_indices], label="Bottom")
+	plt.title("Nucleosome disorganization (entropy)")
+	plt.legend()
--- src/compute_chromatin_entropies.py
+++ src/compute_chromatin_entropies.py
@@ -1,71 +1,78 @@
 
-# Main class to compute the chromatin occupancies for all genes for yl2 dataset
-
-# append to system path for access to src files
-import sys
-sys.path.append('.')
-
-import pandas as pd
-from src.timer import Timer
-from src.chromatin_metrics import ChromatinMetrics, fragment_lengths_definitions
+# Obsolete
 
 
-def compute_rep2_chrom_entropies():
-	"""Main script to read through all replicate 2 filenames
-	collect the gene counts and save to disk"""
+
+
+
+
+# # Main class to compute the chromatin occupancies for all genes for yl2 dataset
+
+# # append to system path for access to src files
+# import sys
+# sys.path.append('.')
+
+# import pandas as pd
+# from src.timer import Timer
+# from src.chromatin_metrics import ChromatinMetrics, fragment_lengths_definitions
+
+
+# def compute_rep2_chrom_entropies():
+# 	"""Main script to read through all replicate 2 filenames
+# 	collect the gene counts and save to disk"""
 		
-	replicate2_filenames = [(0, '../_archive/data/bam/cell_cycle/mnase/DMAH82_MNase_rep2_0_min.bam'),
-							 (10, '../_archive/data/bam/cell_cycle/mnase/DMAH83_MNase_rep2_10_min.bam'),
-							 (20, '../_archive/data/bam/cell_cycle/mnase/DMAH84_MNase_rep2_20_min.bam'),
-							 (30, '../_archive/data/bam/cell_cycle/mnase/DMAH85_MNase_rep2_30_min.bam'),
-							 (40, '../_archive/data/bam/cell_cycle/mnase/DMAH86_MNase_rep2_40_min.bam'),
-							 (50, '../_archive/data/bam/cell_cycle/mnase/DMAH87_MNase_rep2_50_min.bam'),
-							 (60, '../_archive/data/bam/cell_cycle/mnase/DMAH88_MNase_rep2_60_min.bam'),
-							 (70, '../_archive/data/bam/cell_cycle/mnase/DMAH89_MNase_rep2_70_min.bam'),
-							 (80, '../_archive/data/bam/cell_cycle/mnase/DMAH90_MNase_rep2_80_min.bam'),
-							 (90, '../_archive/data/bam/cell_cycle/mnase/DMAH91_MNase_rep2_90_min.bam'),
-							 (100, '../_archive/data/bam/cell_cycle/mnase/DMAH92_MNase_rep2_100_min.bam'),
-							 (110, '../_archive/data/bam/cell_cycle/mnase/DMAH93_MNase_rep2_110_min.bam'),
-							 (120, '../_archive/data/bam/cell_cycle/mnase/DMAH94_MNase_rep2_120_min.bam'),
-							 (130, '../_archive/data/bam/cell_cycle/mnase/DMAH95_MNase_rep2_130_min.bam'),
-							 (140, '../_archive/data/bam/cell_cycle/mnase/DMAH96_MNase_rep2_140_min.bam')]
+# 	replicate2_filenames = [(0, '../_archive/data/bam/cell_cycle/mnase/DMAH82_MNase_rep2_0_min.bam'),
+# 							 (10, '../_archive/data/bam/cell_cycle/mnase/DMAH83_MNase_rep2_10_min.bam'),
+# 							 (20, '../_archive/data/bam/cell_cycle/mnase/DMAH84_MNase_rep2_20_min.bam'),
+# 							 (30, '../_archive/data/bam/cell_cycle/mnase/DMAH85_MNase_rep2_30_min.bam'),
+# 							 (40, '../_archive/data/bam/cell_cycle/mnase/DMAH86_MNase_rep2_40_min.bam'),
+# 							 (50, '../_archive/data/bam/cell_cycle/mnase/DMAH87_MNase_rep2_50_min.bam'),
+# 							 (60, '../_archive/data/bam/cell_cycle/mnase/DMAH88_MNase_rep2_60_min.bam'),
+# 							 (70, '../_archive/data/bam/cell_cycle/mnase/DMAH89_MNase_rep2_70_min.bam'),
+# 							 (80, '../_archive/data/bam/cell_cycle/mnase/DMAH90_MNase_rep2_80_min.bam'),
+# 							 (90, '../_archive/data/bam/cell_cycle/mnase/DMAH91_MNase_rep2_90_min.bam'),
+# 							 (100, '../_archive/data/bam/cell_cycle/mnase/DMAH92_MNase_rep2_100_min.bam'),
+# 							 (110, '../_archive/data/bam/cell_cycle/mnase/DMAH93_MNase_rep2_110_min.bam'),
+# 							 (120, '../_archive/data/bam/cell_cycle/mnase/DMAH94_MNase_rep2_120_min.bam'),
+# 							 (130, '../_archive/data/bam/cell_cycle/mnase/DMAH95_MNase_rep2_130_min.bam'),
+# 							 (140, '../_archive/data/bam/cell_cycle/mnase/DMAH96_MNase_rep2_140_min.bam')]
 
-	save_filename = 'output/all_chromatin_gene_entropies_rep2.csv'
+# 	save_filename = 'output/all_chromatin_gene_entropies_rep2.csv'
 
-	# Configure the length spans we are going to collect
-	small_frag_span, mid_frag_span, nucleosome_len_span = fragment_lengths_definitions()
+# 	# Configure the length spans we are going to collect
+# 	small_frag_span, mid_frag_span, nucleosome_len_span = fragment_lengths_definitions()
 
-	# Gene counts for all time points
-	all_gene_entropies = pd.DataFrame()
+# 	# Gene counts for all time points
+# 	all_gene_entropies = pd.DataFrame()
 
-	# Timer initialization
-	timer = Timer()
-	timer.start()
+# 	# Timer initialization
+# 	timer = Timer()
+# 	timer.start()
 
-	# Iterate through each bam file/timepoint
-	for timepoint, filename in replicate2_filenames:
+# 	# Iterate through each bam file/timepoint
+# 	for timepoint, filename in replicate2_filenames:
 
-		print(f"Computing the entropy scores for the time point {timepoint}, the filepath is: {filename}")
+# 		print(f"Computing the entropy scores for the time point {timepoint}, the filepath is: {filename}")
 
-		# Read the bam file
-		print(f"Reading in the bam file...")
-		chromatin_metrics = ChromatinMetrics(nucleosome_len_span, mid_frag_span, small_frag_span)
-		chromatin_metrics.read_bam_file(filename, timepoint)
+# 		# Read the bam file
+# 		print(f"Reading in the bam file...")
+# 		chromatin_metrics = ChromatinMetrics(nucleosome_len_span, mid_frag_span, small_frag_span)
+# 		chromatin_metrics.read_bam_file(filename, timepoint)
 		
-		cur_entropies = chromatin_metrics.compute_all_gene_entropies()
+# 		cur_entropies = chromatin_metrics.compute_all_gene_entropies()
 
-		# Append the gene counts to the super list for each timepoint
-		cur_entropies['time'] = timepoint
-		all_gene_entropies = pd.concat([all_gene_entropies, cur_entropies])
-		all_gene_entropies.to_csv(save_filename)
+# 		# Append the gene counts to the super list for each timepoint
+# 		cur_entropies['time'] = timepoint
+# 		all_gene_entropies = pd.concat([all_gene_entropies, cur_entropies])
+# 		all_gene_entropies.to_csv(save_filename)
 
-		print(f"Finished time point {timepoint} - {timer.get_time()}")
+# 		print(f"Finished time point {timepoint} - {timer.get_time()}")
 
-	print(f"Completed in {timer.get_time()}")
+# 	print(f"Completed in {timer.get_time()}")
 
-	all_gene_entropies.to_csv(save_filename)
-	print(f"Saved to file: {save_filename}")
+# 	all_gene_entropies.to_csv(save_filename)
+# 	print(f"Saved to file: {save_filename}")
 
 
-if __name__ == '__main__':
-	compute_rep2_chrom_entropies()
+# if __name__ == '__main__':
+# 	compute_rep2_chrom_entropies()
