860388fe14f9c11dffa2cb77066ccc79349f1748
Trung Tran <trung.tran@duke.edu>
2025-02-27 16:27:44 -0500

    More epochs

--- scripts/submit_deconvolve_replication.sh
+++ scripts/submit_deconvolve_replication.sh
@@ -3,7 +3,7 @@
 
 OUTDIR=output/prototype_pipeline
 
-NUM_EPOCHS=1000
+NUM_EPOCHS=10000
 
 CHROM=4
 REPLICATE=1
@@ -15,3 +15,4 @@ REPLICATE=2
 
 ARGS="replication ${OUTDIR} ${REPLICATE} ${CHROM} ${NUM_EPOCHS}"
 sbatch -D ./slurm-logs/ --job-name="rep${REPLICATE}_${ITR}" -p compsci --export="PYFILE=pipeline/run_pipeline.py,ARGS=$ARGS" scripts/cpu_job.sh
+
