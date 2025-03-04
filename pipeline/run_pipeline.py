cfdd7cb332ee92ab40bd4c42e437a5695c7d45b9
Trung Tran <trung.tran@duke.edu>
2025-03-04 14:49:33 -0500

    Fix missing variable

--- pipeline/run_pipeline.py
+++ pipeline/run_pipeline.py
@@ -63,9 +63,10 @@ def main():
 
 		timer = Timer()
 
-		output_directory = 'output/prototype_pipeline_subset/'
 		save_genes_directory = f"{output_directory}/genes_deconvolution/"
 		mkdirs_safe([save_genes_directory])
+
+        runner = CombinedDeconvolveGeneExpressionRunner(output_directory)
 
 		index = 0
 		for _, gene in genes.iterrows():
