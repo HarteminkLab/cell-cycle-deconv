3d7438ecc45860bab3688fb1a98a9ed8db985a04
Trung Tran <trung.tran@duke.edu>
2025-04-23 16:30:12 -0400

    Adjust script typo

--- scripts/submit_run_pipeline.sh
+++ scripts/submit_run_pipeline.sh
@@ -55,7 +55,7 @@ OUTDIR=output/prototype_pipeline_subset
 
 # -------------- No daughter-specificity ------------------
 
-ARGS="deconvolve_chromatin_full_no_daughter ${OUTDIR}"
+ARGS="deconvolve_chromatin_no_daughter ${OUTDIR}"
 sbatch -a 0-999%24 -D ./slurm-logs/ --job-name="nodg1_1" -p compsci --export="PYFILE=pipeline/run_pipeline.py,ARGS=$ARGS,BATCH=0" scripts/cpu_genomicarray_job.sh
 sbatch -a 0-216%24 -D ./slurm-logs/ --job-name="nodg1_2" -p compsci --export="PYFILE=pipeline/run_pipeline.py,ARGS=$ARGS,BATCH=1" scripts/cpu_genomicarray_job.sh
 
