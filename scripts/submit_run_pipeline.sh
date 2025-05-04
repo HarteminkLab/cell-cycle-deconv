e7e1d2422263794a66925de5d94603bb7fa2e14f
Trung Tran <trung.tran@duke.edu>
2025-05-03 20:00:35 -0400

    Add second draft, removal of sigma0 learning

--- scripts/submit_run_pipeline.sh
+++ scripts/submit_run_pipeline.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 # Script to create jobs to create locus plots for all genes across all chromosomes
 
-OUTDIR=output/draft1_run/
+OUTDIR=output/draft2_run/
 
 # ---------- Replication deconvolution ----------------
 
-# NUM_EPOCHS=1000
-# CHROM=4
-# REPLICATE=1
+NUM_EPOCHS=200
+CHROM=4
+REPLICATE=1
 
-# ARGS="replication ${OUTDIR} ${REPLICATE} ${CHROM} ${NUM_EPOCHS} True"
-# sbatch -D ./slurm-logs/ --job-name="rep${REPLICATE}" -p compsci --export="PYFILE=pipeline/run_pipeline.py,ARGS=$ARGS" scripts/cpu_job.sh
-
-# REPLICATE=2
-# ARGS="replication ${OUTDIR} ${REPLICATE} ${CHROM} ${NUM_EPOCHS} True"
-# sbatch -D ./slurm-logs/ --job-name="rep${REPLICATE}" -p compsci --export="PYFILE=pipeline/run_pipeline.py,ARGS=$ARGS" scripts/cpu_job.sh
-
-ARGS="combined_replication ${OUTDIR}"
+ARGS="replication ${OUTDIR} ${REPLICATE} ${CHROM} ${NUM_EPOCHS} True"
 sbatch -D ./slurm-logs/ --job-name="rep${REPLICATE}" -p compsci --export="PYFILE=pipeline/run_pipeline.py,ARGS=$ARGS" scripts/cpu_job.sh
+
+REPLICATE=2
+ARGS="replication ${OUTDIR} ${REPLICATE} ${CHROM} ${NUM_EPOCHS} True"
+sbatch -D ./slurm-logs/ --job-name="rep${REPLICATE}" -p compsci --export="PYFILE=pipeline/run_pipeline.py,ARGS=$ARGS" scripts/cpu_job.sh
+
+# ARGS="combined_replication ${OUTDIR}"
+# sbatch -D ./slurm-logs/ --job-name="rep${REPLICATE}" -p compsci --export="PYFILE=pipeline/run_pipeline.py,ARGS=$ARGS" scripts/cpu_job.sh
 
 # ---------- Expression deconvolution ----------------
 
