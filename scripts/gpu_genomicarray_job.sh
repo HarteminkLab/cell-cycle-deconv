3b284fcdc02a36ec1c8fbe71c462b42cc410076d
Trung Tran <trung.tran@duke.edu>
2025-05-16 16:29:47 -0400

    Add genomic array gpu job in preparation for full genome deconvolution runs on the gpu

--- scripts/gpu_genomicarray_job.sh
+++ scripts/gpu_genomicarray_job.sh
@@ -4,7 +4,7 @@
 #SBATCH --gres=gpu:a5000:1
 
 # Example run:
-# sbatch -D ./slurm-logs/ --export="PYFILE=src/deconvolve_chromatin_runner.py,ARGS='output orfname'" scripts/gpu_script.sh
+# sbatch -D ./slurm-logs/ --export="PYFILE=src/deconvolve_chromatin_runner.py,BATCH=1,ARGS='output'" scripts/cpu_script.sh
 
 cd /usr/xtmp/tqtran/deconvolution-project
 
@@ -18,11 +18,13 @@ conda activate chromatin-deconvolution
 
 export MOSEKLM_LICENSE_FILE=/home/users/tqt3/mosek/mosek.lic
 
-echo "bash: outputs: $ARGS"
+# Batches of 1000 genes, so multiply to get the gene index
+INDEX=$((1000*$BATCH+$SLURM_ARRAY_TASK_ID))
+
+echo "bash: outputs: $ARGS index: $INDEX batch: $BATCH array_index: $SLURM_ARRAY_TASK_ID"
 
 # Run the python command with the task id (array index) as the last argument
-python $PYFILE $ARGS
+python $PYFILE $ARGS $INDEX
 
 echo $(date)
-echo "cpu_job.sh: completed job, date: $(date)"
-
+echo "cpu_genearray_job.sh: completed job, date: $(date)"
